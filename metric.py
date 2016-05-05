ea79aab45f49674a55533a48c5f44161d5c4e940
vovadenisov <vovadenisov@yandex.ru>
2016-05-06 00:54:01 +0300

    fix

--- metric.py
+++ metric.py
@@ -4,23 +4,34 @@ import socket
 import datetime
 import time
 import requests
+from subprocess import call
+import os
+
+#perl -e '$| = 1; while() { $d = `uptime`; ($la) = $d =~ /load average: (.*?),/; $c = qq{echo local_local.load_average $la }.time().qq{ | nc localhost 2003}; system($c); print "$c\n"; sleep 1;}'
 
 
 requests_ = 0
+TCP_IP = "95.213.203.218"
+TCP_PORT = 2003
+#sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+#sock.connect((TCP_IP, TCP_PORT))
 while True:
     cpu = psutil.cpu_percent()
     result = requests.get("http://127.0.0.1/nginx_status")
     text_list = result.text.split("\n")
-    request_count = int(text_list[3].split(" ")[3])
+    request_count = int(text_list[2].split(" ")[3])
     rpc = request_count - requests_
     requests_ = request_count
     dt = datetime.datetime.now()
     timestamp = time.mktime(dt.timetuple())
-    udp_ip = "95.213.251.22"
-    udp_port = 2003
-    message = "{0} {1} {2}".format("server_1.cpu", cpu, timestamp)
-    sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-    sock.sendto(message, (udp_ip, udp_port))
-    message = "{0} {1} {2}".format("server_1.rpc", rpc, timestamp)
-    sock.sendto(message, (udp_ip, udp_port))
+    message = "{0} {1} {2}".format("serverone.cpu", cpu, int(timestamp))
+    print "echo {0} | nc {1} {2}".format(message, TCP_IP, TCP_PORT)
+    #call(["echo {0} | nc {1} {2}".format(message, TCP_IP, TCP_PORT)])
+    #sock.send(message)
+    os.system("echo {0} | nc {1} {2}".format(message, TCP_IP, TCP_PORT))
+    message = "{0} {1} {2}".format("serverone.rpc", rpc, timestamp)
+    os.system("echo {0} | nc {1} {2}".format(message, TCP_IP, TCP_PORT))
+    #call(["echo {0} | nc {1} {2}".format(message, TCP_IP, TCP_PORT)])
+    #sock.send(message)
     sleep(1)
+sock.close()
